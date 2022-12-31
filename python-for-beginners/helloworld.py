3829510e5bcc57eec283ee660dc42c793d7a607d
Alfa Marine <alfamarine.ferreira@gmail.com>
2022-12-31 17:07:44 -0300

    add first code

---
+++ python-for-beginners/helloworld.py
@@ -0,0 +1,28 @@
+# Print and input
+
+## Basic print
+print('Hello world!')
+
+## Print in multiple lines
+print('Hello')
+print('world')
+print('Hello\nworld')
+
+## Printing variable value
+variable = 'Hello world!'
+print(variable)
+
+## Input
+name = input('What is your name? ')
+print('Hello', name)
+
+## Using print to debug code
+print('Adding numbers')
+x = 3 + 5
+print('Dividing numbers')
+
+# Commenting out the line of code causing error
+# y = x / 0
+y = x / 2
+print('End of calculations')
+
